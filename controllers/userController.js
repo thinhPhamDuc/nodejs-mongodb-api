24b663173ef6ab024919b1fe3dd9769947cd6f02
thinhPhamDuc <phamducthinhbeo@gmail.com>
2023-07-13 17:32:14 +0700

    update function sendmail

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt');
 const { User } = require('../models/userModel');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
 
 // user 
 const register = asyncHandler(async (req, res) => {
@@ -49,7 +50,86 @@ const login = asyncHandler(async (req, res) => {
 })
 
 
+const profile = asyncHandler(async (req, res) => {
+  try {
+    const token = req.header('authorization');
+    const data = extractUserId(token);
+    const user = await User.findById(data.userId);
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).send('User not found')
+    }
+
+  } catch (error) {
+      res.status(400).json({ error });
+  }
+})
+
+const extractUserId = (token) => {
+  return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+};
+
+function generateOTP() {
+  // Generate a random 6-digit OTP (you can adjust the length as needed)
+  return Math.floor(100000 + Math.random() * 900000);
+}
+
+const sendingOTPMail = (email) => {
+  const otp = generateOTP(); // Generate OTP
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "phamducthinhbeo@gmail.com",
+        pass: "ztldthwwhvivvxwl"
+    }
+  });
+
+  const mailOptions = {
+    from: 'phamducthinhbeo@gmail.com', // Sender's email address
+    to: email, // Recipient's email address
+    subject: 'OTP Verification',
+    text: `Your OTP is: ${otp}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log('Error occurred while sending email:', error.message);
+    } else {
+      console.log('Email sent:', info.response);
+    }
+  });
+
+  return otp; // Return the generated OTP
+};
+
+
+const update = asyncHandler(async (req, res) => {
+  try {
+    const token = req.header('authorization');
+    const data = extractUserId(token);
+
+    if (req.body.email) {
+      const otp = sendingOTPMail(req.body.email); 
+      console.log(otp);
+      otpStore[data.userId] = otp;
+    }
+
+    const user = await User.findByIdAndUpdate(data.userId, req.body);
+    if (!user) {
+      return res.status(404).json({ message: 'Cannot find this user' });
+    }
+
+    const updatedUser = await User.findById(data.userId);
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
 module.exports = {
     register,
-    login
+    login,
+    profile,
+    update
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const { register,login } = require('../controllers/userController');
+const { register,login,profile, update } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', register)
 
 router.post('/login', login)
 
+router.get('/profile', profile)
+
+router.put('/profile/update', update)
 
 
 module.exports = router; 
